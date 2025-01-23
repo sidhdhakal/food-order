5ee80a05df37cee6fb8bd1ef011dc358588fb17d
babish9887 <babish9887@gmail.com>
2025-01-23 13:52:59 +0545

    Deleted Unnecesary Data

--- src/Pages/Foodspage.tsx
+++
@@ -1,8 +0,0 @@
-
-const Foodspage = () => {
-  return (
-    <div className="flex text-black rounded-[24px] flex-1 justify-start items-start p-4 bg-zinc-50 overflow-hidden">Foodspage</div>
-  )
-}
-
-export default Foodspage
--- src/Pages/OrderHistory.tsx
+++
@@ -1,8 +0,0 @@
-
-const OrderHistory = () => {
-  return (
-    <div className="flex text-black rounded-[24px] flex-1 justify-start items-start p-4 bg-zinc-50 overflow-hidden">OrderHistory</div>
-  )
-}
-
-export default OrderHistory
