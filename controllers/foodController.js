0ec722b301ceef73a2ab1bdaa46a7e3798d3e135
babish9887 <babish9887@gmail.com>
2025-02-06 14:06:47 +0545

    Added Update and Delete for Food

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -31,3 +31,102 @@ exports.createFood = async (req, res) => {
     });
   }
 };
+
+
+exports.updateFood = async (req, res) => {
+  try {
+    const foodToUpdate = await Food.findById(req.params.id);
+    if (!foodToUpdate) {
+      return res.status(404).json({
+        success: false,
+        message: "Food not found",
+      });
+    }
+
+    if (req.body.name && req.body.name !== foodToUpdate.name) {
+      const existingFood = await Food.findOne({ name: req.body.name });
+      if (existingFood) {
+        return res.status(400).json({
+          success: false,
+          message: "Food with this name already exists",
+        });
+      }
+    }
+    let url=''
+
+    if(!req.body.image.startsWith('https://res.cloudinary.com/'))
+    {
+      await deleteImageFromCloudinary(foodToUpdate.image)
+      url=await uploadToCloudinary(req.body.image)
+    }
+    else{
+      url=req.body.image
+    }
+
+    const updatedFood = await Food.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        ...req.body,
+        image:url,
+        updatedAt: Date.now()
+      },
+      { new: true } 
+    );
+
+
+    if(updatedFood){
+      return res.status(200).json({
+        success: true,
+        message: "Food updated successfully",
+        doc: updatedFood,
+      });
+    }
+
+    return res.status(400).json({
+      success: false,
+      message: "Failed to update food",
+      doc: updatedFood,
+    });
+
+  } catch (err) {
+    console.error(err); 
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error!",
+    });
+  }
+};
+
+exports.deleteFood = async (req, res) => {
+  try {
+    const food = await Food.findByIdAndDelete(req.params.id); 
+    if (food) {
+      const result = await deleteImageFromCloudinary(food.image, 'Food')
+      if(result.success)
+        return res.status(200).json({
+            success: true,
+            message: "Food Deleted successfully",
+            doc: food,
+        });
+
+        return res.status(400).json({
+          success: true,
+          message: "Failed to Delete Image From Cloudinary",
+          doc: food,
+      });
+
+    } else {
+        return res.status(404).json({
+            success: false,
+            message: "food not found",
+        });
+    }
+
+  } catch (err) {
+    console.error(err); 
+    res.status(400).json({
+      success: false,
+      message: "Internal Server Error!",
+    });
+  }
+};
--- routes/food.js
+++ routes/food.js
@@ -7,9 +7,9 @@ const { body, validationResult } = require('express-validator');
 const categoryController = require('../controllers/CategoryController')
 const { protect } = require('../Utils/Protect');
 const { restrictTo } = require('../Utils/RestrictTo');
-const { getAll, deleteOne, updateOne, createOne } = require('../controllers/handlerFactory');
+const { getAll,  updateOne, createOne } = require('../controllers/handlerFactory');
 const Category = require('../models/Category');
-const {createFood} =require('../controllers/foodController')
+const {createFood, updateFood, deleteFood} =require('../controllers/foodController')
 
 
 const storage = multer.memoryStorage();  // Store file in memory (buffer)
@@ -32,46 +32,14 @@ router.delete('/category/deletecategory/:id',
   categoryController.deleteCategory)
 
 
-  router.get('/getfoods',getAll(Food))
+router.get('/getfoods',getAll(Food))
 
 router.post('/createfood',createFood)
 
 router.put('/updatefoodavailability/:id', updateOne(Food))
 
-router.post('/uploadphoto', (req, res) => {
+router.put('/updateFood/:id', updateFood)
 
-  const iamge = req.body.image; // Get the uploaded image path
-
-  cloudinary.uploader.upload(iamge, { resource_type: 'auto', folder: 'Foods' }, (error, result) => {
-    if (error) {
-      console.log(error.message)
-      return res.status(500).json({ error: error.message });
-    }
-    res.status(200).json({
-      message: 'Image uploaded successfully',
-      url: result.secure_url, // URL of the uploaded image
-    });
-  });
-
-});
-
-router.post('/deletephoto', async (req, res) => {
-
-  const imageUrl = req.body.imageUrl; // Get the uploaded image path
-
-  try {
-    const publicId = imageUrl.split('/').slice(-1)[0].split('.')[0];
-    
-    await cloudinary.uploader.destroy(publicId);
-
-    res.status(200).json({ message: "Image deleted successfully" });
-
-  } catch (error) {
-    console.error("Error deleting from Cloudinary:", error);
-    
-    res.status(500).json({ message: "Error deleting image from Cloudinary", error: error.message });
-  }
-});
-
+router.delete('/deletefood/:id',deleteFood)
 
 module.exports = router;
