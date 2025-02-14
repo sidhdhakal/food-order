72ab5408cb2f037a73915304ab8cf3c89454f5a9
babish9887 <babish9887@gmail.com>
2025-02-14 02:22:45 -0800

    Update SECRET KEY

--- Utils/decryptData.js
+++ Utils/decryptData.js
@@ -1,7 +1,7 @@
 const CryptoJS = require('crypto-js');
 
 exports.decryptData=(encryptedData) =>{
-    const bytes = CryptoJS.AES.decrypt(encryptedData, 'sonamkotauko');
+    const bytes = CryptoJS.AES.decrypt(encryptedData,  process.env.RSA_SECRET_KEY);
     const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
     return decryptedData;
 }
