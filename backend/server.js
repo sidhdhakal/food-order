b577312cb488cd814ad4c55493da1f0b269b3f1b
sidhdhakal <siddharthadhakal54@gmail.com>
2025-07-14 16:07:06 +0545

    adding kubernetes

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,6 @@ app.use('/api/dashboard', require('./routes/dashboard'));
 
 // --- Start Server ---
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
